5700396837fabda0ce8022a6cc8c8daf253e4248
Joseph <Josephsipiorski@gmail.com>
2021-04-29 14:08:18 -0500

    fixed dist issue

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -3,6 +3,8 @@ import { RECEIVE_TODOS } from '../actions/todo_actions';
 const todosReducer = (state = {}, action) => {
     Object.freeze(state);
     const nextState = Object.assign({}, state);
+    // added comment
+    
     
     switch (action.type) {
 
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const root = document.getElementById("content");
     const store = configureStore();
     window.store = store;
-    
+    debugger
     ReactDOM.render(<Root store={store}/>, root)
 
     
